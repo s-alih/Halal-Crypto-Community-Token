d8ee234e60bda78451590c8d908f36c9ba6b8476
Salih Poovathingal Hussainar <salihpoovathingalhussainar@Salihs-MacBook-Air.local>
2022-11-22 10:54:52 +0400

    test cases

--- test/HalalToken.test.js
+++ test/HalalToken.test.js
@@ -4,18 +4,53 @@ const { assert, expect } = require("chai");
 describe("Halal Token Test", () => {
   let halalToken;
   const sendValue = ethers.utils.parseEther("1");
-  beforeEach(async () => {
-    let HalalToken = await ethers.getContractFactory("HCC");
-    halalToken = await HalalToken.deploy();
-    await halalToken.deployed();
-  });
 
-  describe("Mint and burn", async () => {
-    it("minting and buring token successfully", async () => {
+  describe("Minting", () => {
+    beforeEach(async () => {
+      let HalalToken = await ethers.getContractFactory("HCC");
+      halalToken = await HalalToken.deploy();
+      await halalToken.deployed();
+    });
+
+    it("minting token successfully", async () => {
       const response = await halalToken.deposit({ value: sendValue });
       assert.equal("1.0", ethers.utils.formatEther(response.value));
-      const response2 = await halalToken.withdraw(1);
-      assert.equal("0.0", ethers.utils.formatEther(response2.value));
+    });
+  });
+
+  describe("Burn", () => {
+    beforeEach(async () => {
+      let HalalToken = await ethers.getContractFactory("HCC");
+      halalToken = await HalalToken.deploy();
+      await halalToken.deployed();
+      const response = await halalToken.deposit({ value: sendValue });
+      assert.equal("1.0", ethers.utils.formatEther(response.value));
+    });
+
+    it("Token burn successfully", async () => {
+      const response = await halalToken.withdraw(1);
+      assert.equal("0.0", ethers.utils.formatEther(response.value));
+    });
+  });
+
+  describe("Send Token", () => {
+    beforeEach(async () => {
+      let HalalToken = await ethers.getContractFactory("HCC");
+      halalToken = await HalalToken.deploy();
+      await halalToken.deployed();
+      const response = await halalToken.deposit({ value: sendValue });
+      assert.equal("1.0", ethers.utils.formatEther(response.value));
+    });
+
+    it("Token send successfully", async () => {
+      let randomAddress = "0x5ddb49f23a2e5d83f348a0287085056a0612b365";
+      const response = await halalToken.transfer(randomAddress, 90000000000000);
+      let balance = await halalToken.balanceOf(randomAddress);
+
+      assert.equal(
+        90000000000000,
+        ethers.utils.formatEther(balance) * 10 ** 18
+      );
     });
   });
 });
